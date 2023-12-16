9911dcbb5cc7758d3f5269af8d8d4d510b3b648f
Ricas78 <145056468+Ricas78@users.noreply.github.com>
2023-12-16 19:50:21 -0300

    Update validacaoRFID2.py

--- validacaoRFID2.py
+++ validacaoRFID2.py
@@ -4,9 +4,8 @@ import RPi.GPIO as GPIO
 
 # desabilita os avisos
 GPIO.setwarnings(False)
-
-GPIO.stmode(GPIO, BCM)
-
+# configura os pinos GPIOs da rasp que serão utilizados como pinos de saída
+GPIO.setmode(GPIO, BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
@@ -19,8 +18,11 @@ ID = {'712936067078'}
 
 try:
     while True:  # loop
+        # cria as variáveis "id" e "texto", e as atribui as leituras da id e do texto coletado da tag pelo leitor, respectivamente
         id, texto = leitor.read()
+        # passa o numero de id para string
         id = str(id)
+        # se o numero do id for o mesmo armazenado na variável ID, pisca o led verde e informa a mensagem de "acesso liberado"
         if id in ID:
             GPIO.output(23, GPIO.HIGH)
             print("acesso liberado")
@@ -28,6 +30,7 @@ try:
             GPIO.output(23, GPIO.LOW)
             sleep(1)
         else:
+           # se não, pisca o led vermelho e informa a mensagem de "acesso negado"
             GPIO.output(22, GPIO.HIGH)
             print("acesso negado")
             sleep(3)
@@ -36,3 +39,4 @@ try:
 
 except KeyboardInterrupt:
     GPIO.cleanup()  # toda vez que interrompermos o programa com crtl C o pino vai ser limpado automaticmanete
+
