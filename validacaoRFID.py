69ad5249a8162d7b64c77a2f4e6b8a4c50ec8c44
Ricas78 <145056468+Ricas78@users.noreply.github.com>
2023-12-16 19:48:57 -0300

    Update validacaoRFID.py

--- validacaoRFID.py
+++ validacaoRFID.py
@@ -4,9 +4,8 @@ import RPi.GPIO as GPIO
 
 # desabilita os avisos
 GPIO.setwarnings(False)
-
-GPIO.stmode(GPIO, BCM)
-
+# configura os pinos GPIOs da rasp que serão utilizados como pinos de saída
+GPIO.setmode(GPIO, BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
@@ -17,14 +16,18 @@ print("Aproxime a tag do leitor para leitura.")
 
 try:
     while True:  # loop
+        # cria as variáveis "id" e "texto", e as atribui as leituras da id e do texto coletado da tag pelo leitor, respectivamente
         id, texto = leitor.read()
+        # condicional para verificar se o id do cartão é o mesmo do salvo no código
         if id == 712936067078:
+            # pisca o led verde e informa a mensagem de "acesso liberado" se a condição for satisfeita
             GPIO.output(23, GPIO.HIGH)
             print("acesso liberado")
             sleep(3)
             GPIO.output(23, GPIO.LOW)
             sleep(1)
         else:
+            # pisca o led vermelho e informa a mensagem de "acesso negado" se a condição não for satisfeita
             GPIO.output(22, GPIO.HIGH)
             print("acesso negado")
             sleep(3)
